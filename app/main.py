f02e2cd3c725f4180d19edbd88a127921bdc37b9
coommark <coommark@gmail.com>
2025-07-29 11:41:35 +0100

    added CORS

--- app/main.py
+++ app/main.py
@@ -3,12 +3,22 @@ from .routers import users, assessments, dashboard
 from .dependencies import Base, engine
 
 import logging
+from fastapi.middleware.cors import CORSMiddleware
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(assessments.router)
